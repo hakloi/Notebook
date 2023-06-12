29178905303a7db307a05b7c9aa5f7c0a23d9d22
Violetta <murasakitta@gmail.com>
2023-06-12 15:22:36 +0300

    note get set

--- note.py
+++ note.py
@@ -4,3 +4,27 @@ class Note:
         self.title = title
         self.body = body
         self.date = date
+        
+    def get_id(self):
+        return self.id
+
+    def get_title(self):
+        return self.title
+
+    def get_body(self):
+        return self.body
+
+    def get_date(self):
+        return self.date
+
+    def set_id(self, new_id):
+        self.id = new_id
+
+    def set_title(self, new_title):
+        self.title = new_title
+
+    def set_body(self, new_body):
+        self.body = new_body
+
+    def set_date(self, new_date):
+        self.date = new_date
